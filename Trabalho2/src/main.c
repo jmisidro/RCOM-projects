02b1618157554755f6a65f9facbdfc34b1412159
José Miguel Isidro <93340308+zmiguel2011@users.noreply.github.com>
2023-11-15 10:07:44 +0000

    added experiment 1 logs

--- Trabalho2/src/main.c
+++ Trabalho2/src/main.c
@@ -3,8 +3,8 @@
 #include "app.h"
 
 int main(int argc, char *argv[]) {
-    if(argc != 2) { 
-        printf("Usage: %s %s \n", argv[0], "ftp://{user}:{password}@{host}/{url-path}");
+    if(argc != 2) {                         //<user>:<password>@<host>:<port>/<url-path>
+        printf("Usage: %s %s \n", argv[0], "ftp://<user>:<password>@<host>/<url-path>");
         return -1;
     }
        
